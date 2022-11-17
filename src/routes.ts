48b0c5db417c638193c2531a48671f294ab4574e
Frank <pacog879@hotmail.com>
2022-11-17 11:40:27 -0600

    consolidated routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,9 @@
 import { Router } from 'express';
 
 import DemonRouter from './routes/demons'
-import P5Router from './routes/p5'
-import P4Router from './routes/p4'
-import P3Router from './routes/p3'
 
 const Routes = Router();
 
 Routes.use('/api/demons', DemonRouter);
-Routes.use('/api/p5/demons', P5Router);
-Routes.use('/api/p4/demons', P4Router);
-Routes.use('/api/p3/demons', P3Router);
 
 export default Routes;
--- src/routes/demons.ts
+++ src/routes/demons.ts
@@ -7,11 +7,26 @@ router.get('/all', (_req, res) => {
   res.send(megaten.demons);
 })
 
+router.get('/:gameName/all', (req, res) => {
+  const gameName = req.params.gameName;
+  const demons = megaten.demons.filter(demon => demon.game === gameName);
+  res.send(demons);
+})
+
 router.get('/:pageNumber', (req, res) => {
   const pageNumber = parseInt(req.params.pageNumber, 10);
   const demons = megaten.demons.slice((pageNumber - 1) * 20, pageNumber * 20);
   const totalPages = Math.ceil(megaten.demons.length / 20);
   res.send({ demons, totalPages });
+})
+
+router.get('/:gameName/:pageNumber', (req, res) => {
+  const gameName = req.params.gameName;
+  const pageNumber = parseInt(req.params.pageNumber, 10);
+  const demons = megaten.demons.filter(demon => demon.game === gameName);
+  const demonsOnPage = demons.slice((pageNumber - 1) * 20, pageNumber * 20);
+  const totalPages = Math.ceil(demons.length / 20);
+  res.send({ demons: demonsOnPage, totalPages });
 })
 
 export default router
